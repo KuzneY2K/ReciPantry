2424381c128f55e39603ec00d5f4624ce354a2fc
Lincoln Marler <141679750+lincmarler@users.noreply.github.com>
2023-09-26 14:28:08 -0600

    account page

--- ReciPantry.client/src/pages/AccountPage.vue
+++ ReciPantry.client/src/pages/AccountPage.vue
@@ -1,8 +1,31 @@
 <template>
-  <div class="about text-center">
-    <h1>Welcome {{ account.name }}</h1>
-    <img class="rounded" :src="account.picture" alt="" />
-    <p>{{ account.email }}</p>
+  <div class="container">
+    <section class="row">
+      <!-- STUB Account info -->
+      <div class="about text-center mt-5 col-12">
+        <h1 class="p-2">My Account</h1>
+        <img class="rounded" :src="account.picture" alt="" />
+        <h2 class="mt-3">{{ account.name }}</h2>
+      </div>
+      <!-- STUB Account page buttons -->
+      <div class="col-12 text-center mt-5">
+        <button class="btn btn-outline-dark">
+          <h1>EDIT ACCOUNT</h1>
+        </button>
+      </div>
+      <div class="col-12 text-center mt-3">
+        <button class="btn btn-outline-dark">
+          <h1>PREFERENCES</h1>
+        </button>
+      </div>
+      <div class="col-12 text-center mt-3">
+        <button class="btn btn-outline-dark">
+          <h1>MY RECIPES</h1>
+        </button>
+      </div>
+    </section>
+    <!-- STUB  insert recipe card here-->
+
   </div>
 </template>
 
@@ -20,6 +43,6 @@ export default {
 
 <style scoped>
 img {
-  max-width: 100px;
+  max-width: 200px;
 }
 </style>
