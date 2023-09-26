e37312d9984c817069b8cbfcdec55276df0893c7
Mark Kuznetsov <mkuz.fl@gmail.com>
2023-09-26 10:40:02 -0600

    .

--- ReciPantry.client/src/components/Navbar.vue
+++ ReciPantry.client/src/components/Navbar.vue
@@ -1,24 +1,33 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-      <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
-      </div>
-    </router-link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+  <nav class="navbar fixed-bottom navbar-expand-lg navbar-dark bg-white px-3 d-flex flex-row justify-content-center">
+    <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
       aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto">
-        <li>
-          <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
-            About
+    </button> -->
+    <div id="navbarText d-flex flew-row justify-content-around">
+      <ul class="navbar-nav d-flex flex-row justify-content-around">
+        <li class="px-3">
+          <router-link :to="{ name: 'Home' }" class="btn text-success selectable text-uppercase">
+            <img src="https://i.imgur.com/rNYQdS7.png" alt="" height="35">
+          </router-link>
+        </li>
+        <li class="px-3">
+          <router-link :to="{ name: 'Home' }" class="btn text-success selectable text-uppercase">
+            <img src="https://i.imgur.com/HkbRCBE.png" alt="" height="35">
+          </router-link>
+        </li>
+        <li class="px-3">
+          <router-link :to="{ name: 'Home' }" class="btn text-success selectable text-uppercase">
+            <img src="https://i.imgur.com/msy9hgt.png" alt="" height="35">
+          </router-link>
+        </li>
+        <li class="px-3">
+          <router-link :to="{ name: 'Account' }" class="btn text-success selectable text-uppercase">
+            <img src="https://i.imgur.com/3bXnC5K.png" alt="" height="35">
           </router-link>
         </li>
       </ul>
       <!-- LOGIN COMPONENT HERE -->
-      <Login />
     </div>
   </nav>
 </template>
@@ -29,7 +38,6 @@ export default {
   setup() {
     return {}
   },
-  components: { Login }
 }
 </script>
 
@@ -40,12 +48,6 @@ a:hover {
 
 .nav-link {
   text-transform: uppercase;
-}
-
-.navbar-nav .router-link-exact-active {
-  border-bottom: 2px solid var(--bs-success);
-  border-bottom-left-radius: 0;
-  border-bottom-right-radius: 0;
 }
 
 @media screen and (min-width: 768px) {
