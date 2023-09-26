56f08a3e5be11acd2ec68213af95546c43ad7ea1
Mark Kuznetsov <mkuz.fl@gmail.com>
2023-09-26 16:25:14 -0600

    ./

--- ReciPantry.client/src/components/RecipeCard.vue
+++ ReciPantry.client/src/components/RecipeCard.vue
@@ -1,25 +1,29 @@
 <template>
     <!-- Add router link with params being the props.recipe.id -->
+    <RouterLink :to="{ name: 'RecipeDetails', params: {recipeId: props.recipe.id} }">
     <div class="card-container p-0 m-0 rounded rounded-5 my-2 border border-1 border-dark elevation-5 animate__animated animate__fadeInRightBig postion-relative" :style="{ backgroundImage: 'url(' + props.recipe.image + ')' }">
         <h2 class="postion-absolute text-white recipe-title position-absolute ms-3">{{ props.recipe.title }}</h2>
         <div class="container">
         <span class="learnMore position-absolute text-white">Learn More</span><i class="mdi mdi-arrow-right-bold p-0 m-0 fs-1 text-white position-absolute arrow"></i></div>
     </div>
+    </RouterLink>
 </template>
 
 <script>
 import { computed } from 'vue';
 import { AppState } from '../AppState.js';
 import { Recipe } from '../models/Recipe.js';
+import { RouterLink } from 'vue-router';
 
     export default {
-        props: {recipe: {type: [Recipe, Object], required: true}},
-        setup(props){
-            return{
-                props
-            }
-        }
-    }
+    props: { recipe: { type: [Recipe, Object], required: true } },
+    setup(props) {
+        return {
+            props
+        };
+    },
+    components: { RouterLink }
+}
 </script>
 
 <style lang="scss" scoped>
