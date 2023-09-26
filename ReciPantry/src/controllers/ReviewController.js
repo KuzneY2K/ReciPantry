f88ab8596f5ffc22643aae6581bb5423ad8ae2fe
Jason Millhorn <jdmillhorn@gmail.com>
2023-09-26 11:06:38 -0600

    push

---
+++ ReciPantry/src/controllers/ReviewController.js
@@ -0,0 +1,10 @@
+import BaseController from "../utils/BaseController.js";
+
+
+export class ReviewController extends BaseController {
+    constructor() {
+        super('api/reviews')
+        this.router
+
+    }
+}
--- ReciPantry/src/models/Review.js
+++ ReciPantry/src/models/Review.js
@@ -2,8 +2,22 @@ import { Schema } from "mongoose";
 
 
 export const ReviewSchema = new Schema({
-    rating: { type: Number, required: true, min: 1, max: 5, },
-    comment: {},
-    accountId: {},
-    recipeId: {}
+    rating: { type: Number, required: true, min: 1, max: 5 },
+    comment: { type: String, minLength: 1, maxLength: 144 },
+    accountId: { type: Schema.Types.ObjectId, ref: 'Account', required: true },
+    recipeId: { type: Schema.Types.ObjectId, ref: 'Recipe', required: true }
 }, { timestamps: true, toJSON: { virtuals: true } })
+
+ReviewSchema.virtual('profile', {
+    localField: 'accountId',
+    foreignField: 'id',
+    ref: 'Account',
+    justOne: true
+})
+
+ReviewSchema.virtual('recipe', {
+    localField: 'recipeId',
+    foreignField: 'id',
+    ref: 'Recipe',
+    justOne: true
+})
