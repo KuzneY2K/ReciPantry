7225ff315864b4f004c5dbb36a8df0ff3a6509e9
Mark Kuznetsov <mkuz.fl@gmail.com>
2023-09-26 12:39:36 -0600

    .

--- ReciPantry.client/src/components/Navbar.vue
+++ ReciPantry.client/src/components/Navbar.vue
@@ -2,22 +2,22 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-white px-3 d-flex flex-row justify-content-center">
     <div id="navbarText d-flex flew-row justify-content-around">
       <ul class="navbar-nav d-flex flex-row justify-content-around">
-        <li class="px-3">
+        <li class="px-1">
           <router-link :to="{ name: 'Home' }" class="btn text-success selectable text-uppercase">
             <img src="https://i.imgur.com/rNYQdS7.png" alt="" height="35">
           </router-link>
         </li>
-        <li class="px-3">
+        <li class="px-1">
           <router-link :to="{ name: 'Home' }" class="btn text-success selectable text-uppercase">
             <img src="https://i.imgur.com/HkbRCBE.png" alt="" height="35">
           </router-link>
         </li>
-        <li class="px-3">
+        <li class="px-1">
           <router-link :to="{ name: 'Home' }" class="btn text-success selectable text-uppercase">
             <img src="https://i.imgur.com/msy9hgt.png" alt="" height="35">
           </router-link>
         </li>
-        <li class="px-3">
+        <li class="px-1">
           <router-link :to="{ name: 'Account' }" class="btn text-success selectable text-uppercase">
             <img src="https://i.imgur.com/3bXnC5K.png" alt="" height="35">
           </router-link>
