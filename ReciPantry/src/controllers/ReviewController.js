adc54ed8fa79e3554098a2eb077b461ccd53a406
Jason Millhorn <jdmillhorn@gmail.com>
2023-09-26 11:14:01 -0600

    push

--- ReciPantry/src/controllers/ReviewController.js
+++ ReciPantry/src/controllers/ReviewController.js
@@ -1,10 +1,23 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
 import BaseController from "../utils/BaseController.js";
+import { reviewService } from "../services/ReviewService.js";
 
 
 export class ReviewController extends BaseController {
     constructor() {
         super('api/reviews')
         this.router
+            .get('/:recipeId', this.getReviewsByRecipe)
+            .use(Auth0Provider.getAuthorizedUserInfo)
+    }
 
+    async getReviewsByRecipe(req, res, next) {
+        try {
+            const recipeId = req.params.recipeId
+            const reviews = await reviewService.getReviewsByRecipe(recipeId)
+            res.send(reviews)
+        } catch (error) {
+            next(error)
+        }
     }
 }
