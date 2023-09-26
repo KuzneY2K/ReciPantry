b801c0e528fcf66f7163536796959f0dd86f52d4
Mark Kuznetsov <mkuz.fl@gmail.com>
2023-09-26 13:45:34 -0600

    ./

--- ReciPantry.client/src/pages/HomePage.vue
+++ ReciPantry.client/src/pages/HomePage.vue
@@ -1,11 +1,22 @@
 <template>
+  <!-- Landing Text -->
   <section class="row p-0 m-0 container-fluid">
     <div class="col-12 p-0 m-0">
       <h1 class="home-title postion-absolute ms-4 mt-4"><span class="text-success">FIND</span> THE RECIPE THAT <br>SUITS <span class="text-success">YOU</span></h1>
     </div>
   </section>
+  <!-- Search Bar -->
   <section class="row p-0 m-0 container-fluid">
+    <div class="col-12 p-0 m-0 d-flex flex-row justify-content-center">
+      <form>
+        <div class="input-group p-0 m-0 mb-3 mt-4">
+          <span class="input-group-text p-0 m-0 py-1 px-3" id="basic-addon1"><i class="mdi mdi-magnify fs-2"></i></span>
+            <input type="text" class="form-control bg-white" placeholder="Search a recipe" aria-label="Username" aria-describedby="basic-addon1"><button class="btn btn-grad"><img src="https://i.imgur.com/Gh9UJij.png" alt="" height="30"></button>
+          </div>
+      </form>
+    </div>
   </section>
+  <!-- Landing Card -->
   <section class="row p-0 m-0 container-fluid">
     <div class="col-12 p-0 m-0">
       <!-- COMPLETELY SEPARATE FROM RECIPE CARDS. -->
@@ -28,15 +39,24 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
+<style scoped lang="scss">         
+.btn-grad {
+            background-color: #68af8e;
+          }
 .home {
   display: grid;
   height: 80vh;
   place-content: center;
   text-align: center;
   user-select: none;
+
+           
+
+         
   .home-card {
     width: 50vw;
+
+    
 
     >img {
       height: 200px;
