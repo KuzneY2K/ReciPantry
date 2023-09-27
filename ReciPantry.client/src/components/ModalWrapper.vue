679c6c58e36d84275a0e9503d222f23981784f18
Lincoln Marler <141679750+lincmarler@users.noreply.github.com>
2023-09-27 10:48:07 -0600

    modal wrapper

---
+++ ReciPantry.client/src/components/ModalWrapper.vue
@@ -0,0 +1,40 @@
+<template>
+    <!-- STUB modal button -->
+    <button v-if="showButton" type="button" class="btn btn-outline dark" data-bs-toggle="modal" :data-bs-target="`#${id}`">
+        <slot name="button">
+            open {{ id }} modal
+        </slot>
+    </button>
+    <!-- STUB modal body -->
+    <div class="modal fade" :id="id" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-xl" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <slot name="header">
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                        </button>
+                    </slot>
+                </div>
+                <div class="modal-body text-dark">
+                    <slot name="body">
+
+                    </slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {id: {type: String, required: true}, showButton: {type: Boolean, default: true}},
+setup() {
+  return {};
+},
+};
+</script>
+
+
+<style>
+</style>
