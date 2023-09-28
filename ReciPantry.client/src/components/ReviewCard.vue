573676f8f54ba4a7b532798a2ff7d011ebb63de2
Jason Millhorn <jdmillhorn@gmail.com>
2023-09-28 11:46:37 -0600

    push

--- ReciPantry.client/src/components/ReviewCard.vue
+++ ReciPantry.client/src/components/ReviewCard.vue
@@ -1,5 +1,10 @@
 <template>
-  {{ review }}
+  <div class="card-container p-0 m-0 rounded rounded-5 my-2 border border-1 border-dark elevation-5 animate__animated
+    animate__fadeInRightBig position-relative">
+    <h1>
+      {{ review.profile.name }}
+    </h1>
+  </div>
 </template>
 
 <script>
@@ -14,4 +19,12 @@ export default {
 </script>
 
 
-<style></style>
+<style lang="scss" scoped>
+.card-container {
+  width: 90%;
+  height: 7rem;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+}
+</style>
