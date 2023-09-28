4e0e3050be98ed87f700740067028e1ebe7cf75f
Mark Kuznetsov <mkuz.fl@gmail.com>
2023-09-28 15:12:05 -0600

    ./

--- ReciPantry/src/services/GroceriesService.js
+++ ReciPantry/src/services/GroceriesService.js
@@ -1,6 +1,11 @@
 import { dbContext } from "../db/DbContext.js"
+import { BadRequest } from "../utils/Errors.js"
 
 class GroceriesService {
+    async getAllGroceries() {
+        const groceries = await dbContext.Groceries.find().populate('creatorId', 'name id _id')
+        return groceries
+    }
     async addGrocery(groceryData) {
         const newGrocery = await dbContext.Groceries.create(groceryData)
         await newGrocery.populate('creatorId', 'name id _id')
@@ -12,8 +17,16 @@ class GroceriesService {
         return myGroceries
     }
 
-    async removeGrocery(groceryId) {
-
+    async removeGrocery(groceryId, accountId) {
+        const removedGrocery = await dbContext.Groceries.findById(groceryId).populate('creatorId')
+        if (!removedGrocery) {
+            throw new BadRequest('Invalid Grocery')
+        }
+        if (removedGrocery.accountId != accountId) {
+            throw new BadRequest('Unauthorized Request')
+        }
+        removedGrocery.remove()
+        return `Removed ${removedGrocery.id}`
     }
 
 }
