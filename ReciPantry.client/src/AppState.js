e57febfb84278f0ba5b60f0ed4f85852a4d867d1
Mark Kuznetsov <mkuz.fl@gmail.com>
2023-09-28 10:20:33 -0600

    ./

--- ReciPantry.client/src/AppState.js
+++ ReciPantry.client/src/AppState.js
@@ -6,10 +6,10 @@ export const AppState = reactive({
   user: {},
   /** @type {import('./models/Account.js').Account} */
   account: {},
-  /** @type {import('./models/Recipe.js').Recipe{}} */
-  recipes: {},
-  /** @type {import('./models/Recipe.js').Recipe{}} */
-  communityRecipes: {},
+  /** @type {import('./models/Recipe.js').Recipe[{}]} */
+  recipes: [{}],
+  /** @type {import('./models/Recipe.js').Recipe[{}]} */
+  communityRecipes: [{}],
   /** @type {import('./models/Recipe.js').Recipe} */
   randomRecipe: {},
   /** @type {import('./models/Recipe.js').Recipe} */
