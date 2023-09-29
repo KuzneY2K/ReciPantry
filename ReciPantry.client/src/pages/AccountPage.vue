7004f31eda2accadc0047cacf559e8b984ce1d13
Mark Kuznetsov <mkuz.fl@gmail.com>
2023-09-29 16:00:50 -0600

    ./

--- ReciPantry.client/src/pages/AccountPage.vue
+++ ReciPantry.client/src/pages/AccountPage.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="container" v-if="account">
     <section class="row">
-      <div class="mt-3 list-group-item dropdown-item list-group-item-action  text-dark fs-2 col-12 text-end"
-        @click="logout">
-        <span class="selectable p-2">
-          <i class="mdi mdi-logout"></i>
-          Logout
-        </span>
 
-      </div>
       <!-- STUB Account info -->
       <div class="about text-center mt-5 col-12">
         <h1 class="p-2">My Account</h1>
-        <img class="profile-pic" :src="account.picture" alt="" />
+        <img class="profile-pic rounded border border-1 border-black elevation-5" :src="account.picture" alt="" />
         <h2 class="mt-3">{{ account.name }}</h2>
+        <div class="list-group-item dropdown-item list-group-item-action text-dark fs-2 col-12"
+          @click="logout">
+          <span class="selectable p-2 btn btn-success p-0 m-0 px-4 py-2">
+            <i class="mdi mdi-logout"></i>
+            Logout
+        </span>
+        </div>
       </div>
       <!-- STUB Account page buttons -->
       <!-- Edit account modal -->
 
-      <div class="col-12 text-center mt-5">
+      <div class="col-12 text-center">
         <ModalWrapper id="edit-account">
           <template #button>
             <h1 class="fs-2 btn btn-outline-dark">EDIT ACCOUNT <i class="mdi mdi-head"></i></h1>
@@ -45,7 +45,7 @@
           <h1 class="fs-2 pt-2">MY <i class="mdi mdi-bomb"></i> RECIPES </h1>
         </button>
       </div>
-      <div class="collapse" id="myRecipes" v-for="recipe in recipes" :key="recipe.id">
+      <div class="collapse d-flex flex-column align-items-center" id="myRecipes" v-for="recipe in recipes" :key="recipe.id">
         <CommunityRecipeCard :recipe="recipe" />
       </div>
 
@@ -110,8 +110,8 @@ export default {
 
 <style lang="scss" scoped>
 .profile-pic {
-  height: 250px;
-  width: 250px;
+  height: 250px !important;
+  width: 250px !important;
   object-fit: cover;
   object-position: center;
   border-radius: 500px;
