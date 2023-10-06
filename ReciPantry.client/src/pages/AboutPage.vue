4bea3654d039d14e3fdc57276687b3dedd378e6d
Jason Millhorn <jdmillhorn@gmail.com>
2023-10-06 16:18:56 -0600

    centered profile cards

--- ReciPantry.client/src/pages/AboutPage.vue
+++ ReciPantry.client/src/pages/AboutPage.vue
@@ -5,13 +5,13 @@
       <h1 class="text-center mt-4">The Chefs <i class="mdi mdi-chef-hat"></i></h1>
       <div class="p-1 col-10 col-md-2 card mt-4 elevation-5 mx-3" data-aos="fade-right">
         <img src="../assets/img/tyler-pic.jpg" alt="Tyler">
-        <span class="fs-3 ms-2 mt-3">Tyler:</span>
+        <span class="fs-3 text-center ms-2 mt-3">Tyler</span>
         <span class="p-2">Hello, my name is Tyler! I am a prospective full stack developer and a student at Boise
           CodeWorks. After
           graduating High School I attended CSU Channel Islands for a year before realizing I wanted something different
           for myself. I decided to take a leap forward and jump-started my career as a software developer, and it has
           truly been a life changing experience. </span>
-        <div class="links-container fs-1">
+        <div class="links-container text-center fs-1">
           <span class="text-end"><a target="_blank" href="https://github.com/TylerChristiansen22"><i
                 class="text-success mdi mdi-github"></i></a> <a target="_blank"
               href="https://www.linkedin.com/in/tyler-christiansen-58555328a/"><i
@@ -20,12 +20,12 @@
       </div>
       <div class="p-1 m-1 col-10 col-md-2 card mt-4 elevation-5 mx-3" data-aos="fade-left">
         <img src="../assets/img/jason-pic.jpg" alt="Jason">
-        <span class="fs-3 ms-2 mt-3">Jason:</span>
+        <span class="fs-3 text-center ms-2 mt-3">Jason</span>
         <span class="p-2">Born and raised in California, I have always had an interest in computers and the idea of
           building
           applications. My ambition led me to Boise, Idaho to attend Boise CodeWorks. I will pursue a career in web
           development upon completion of the course.</span>
-        <div class="links-container fs-1">
+        <div class="links-container text-center fs-1">
           <span class="text-end"><a target="_blank" href="https://github.com/millho"><i
                 class="text-success mdi mdi-github"></i></a> <a target="_blank"
               href="https://www.linkedin.com/in/jason-millhorn-4aa0a023b/"><i
@@ -34,13 +34,13 @@
       </div>
       <div class="p-1 m-1 col-10 col-md-2 card mt-4 elevation-5 mx-3" data-aos="fade-right">
         <img src="../assets/img/image0.jpeg" alt="Mark">
-        <span class="fs-3 ms-2 mt-3">Mark:</span>
+        <span class="fs-3 ms-2 text-center mt-3">Mark</span>
         <span class="p-2">Hi my name is Mark. Im originally from Florida and got stationed out in Mountain Home, ID while
           serving in the United States Air Force.After my military contract was over I was unsure of what exactly I wanted
           to do with my life and was for some time, lost. That was until one day, I figured I'd give coding a shot and
           discovered my life's true passion. I am very excited for what the future has in store for my self and my
           classmates.</span>
-        <div class="links-container fs-1">
+        <div class="links-container text-center fs-1">
           <span class="text-end"><a target="_blank" href="https://github.com/KuzneY2K"><i
                 class="text-success mdi mdi-github"></i></a> <a target="_blank" href="https://www.linkedin.com/"><i
                 class="text-success mdi mdi-linkedin"></i></a></span>
@@ -48,12 +48,12 @@
       </div>
       <div class="p-1 m-1 col-10 col-md-2 card mt-4 elevation-5 mx-3" data-aos="fade-left">
         <img src="../assets/img/linc-pic.jpeg" alt="Lincoln">
-        <span class="fs-3 ms-2 mt-3">Lincoln:</span>
+        <span class="fs-3 ms-2 text-center mt-3">Lincoln</span>
         <span class="p-2">Raised here in the great state of Idaho, I've spent my first couple adult years working a
           multitude of
           different construction and labor jobs before realizing I needed a more sustaining and steady career. I am now a
           student at Codeworks Boise, excited to take the leap into the Software Development world.</span>
-        <div class="links-container fs-1">
+        <div class="links-container text-center fs-1">
           <span class="text-end"><a target="_blank" href="https://github.com/lincmarler?tab=repositories"><i
                 class="text-success mdi mdi-github"></i></a> <a target="_blank"
               href="www.linkedin.com/in/lincoln-marler-920a34287"><i class="text-success mdi mdi-linkedin"></i></a></span>
