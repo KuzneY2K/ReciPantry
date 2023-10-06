3e3534d1d261373facafd0567adbebb0a3e54765
TylerChristiansen22 <tyguy.tc@gmail.com>
2023-10-06 12:06:58 -0600

    about page skeleton

--- ReciPantry.client/src/pages/AboutPage.vue
+++ ReciPantry.client/src/pages/AboutPage.vue
@@ -1,6 +1,47 @@
 <template>
-  <div class="about">
-    <h1>This is the about page</h1>
+  <div class="container-fluid">
+
+    <div class="about row justify-content-around">
+      <h1 class="mt-1 text-center">The Chefs</h1>
+      <div class="p-1 m-1 col-10 col-md-2 card">
+        <img src="../assets/img/tyler-pic.jpg" alt="Tyler">
+        <span>Bio</span>
+        <span>Links</span>
+      </div>
+      <div class="p-1 m-1 col-10 col-md-2 card">
+        <img src="../assets/img/jason-pic.jpg" alt="Jason">
+        <span>Bio</span>
+        <span>Links</span>
+      </div>
+      <div class="p-1 m-1 col-10 col-md-2 card">
+        <img src="../assets/img/tyler-pic.jpg" alt="Tyler">
+        <span>Bio</span>
+        <span>Links</span>
+      </div>
+      <div class="p-1 m-1 col-10 col-md-2 card">
+        <img src="../assets/img/tyler-pic.jpg" alt="Tyler">
+        <span>Bio</span>
+        <span>Links</span>
+      </div>
+      <h1 class="mt-1 text-center">About The Site</h1>
+      <div class="card col-11">
+        <span class="fs-4">Problem Statement:</span>
+        <span class="fs-6">Find recipes instantly & learn to cook</span>
+        <span class="fs-4">Concepts:</span>
+        <span class="fs-6">Collection of Recipes, User uploaded & "chef" picked</span>
+        <span class="fs-4">Target Audience:</span>
+        <span class="fs-6">Home Cooks / Aspiring Chefs</span>
+        <span class="fs-4">Why:</span>
+        <span class="fs-6">Help Home Cooks -> Eating out is expensive -> Learn to cook better than restaurants</span>
+        <span class="fs-4">The Planning Stage:</span>
+        <span class="fs-6">Figma</span>
+        <span class="fs-6">UML</span>
+        <span class="fs-6">Trello</span>
+        <span class="fs-4">Technologies Used:</span>
+        <span class="fs-6">yadayada</span>
+
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,3 +54,5 @@ export default {
   }
 }
 </script>
+
+<style></style>
