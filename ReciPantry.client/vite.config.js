124ddffc1a65dffd85938abbbd899ea579b757f2
Mark Kuznetsov <mkuz.fl@gmail.com>
2023-10-08 17:06:49 -0600

    ./

--- ReciPantry.client/vite.config.js
+++ ReciPantry.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: '../ReciPantry/client',
+    outDir: '../ReciPantry/www',
     sourcemap: false
   },
   server: {
